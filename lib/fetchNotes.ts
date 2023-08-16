e09f1649c12229a7a5cc2939540de1206473aff9
alfaz86 <hidecode403@gmail.com>
2023-08-16 22:37:29 +0700

    set db

--- lib/fetchNotes.ts
+++ lib/fetchNotes.ts
@@ -1,5 +1,5 @@
 export default async function fetchNotes() {
-    const json_server_url = process.env.NEXT_PUBLIC_JSON_SERVER_URL;
+    const json_server_url = process.env.NEXT_PUBLIC_DB_SERVER_HOST;
     const res = await fetch(`${json_server_url}/notes`)
     const notes: Note[] = await res.json()
 
--- pages/api/notes.ts
+++ pages/api/notes.ts
@@ -1,5 +1,5 @@
 export default async function handler(req: any, res: any) {
-  const json_server_url = process.env.NEXT_PUBLIC_JSON_SERVER_URL;
+  const json_server_url = process.env.NEXT_PUBLIC_DB_SERVER_HOST;
   const response = await fetch(`${json_server_url}/notes`)
   const notes: Note[] = await response.json()
 
