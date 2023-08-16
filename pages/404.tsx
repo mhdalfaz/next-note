c08e64f23ac6ad0a2d2c056570f58a67195f3423
alfaz86 <hidecode403@gmail.com>
2023-08-16 20:04:28 +0700

    change tag <a> with <Link>

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Custom404() {
   return (
     <div className="h-screen text-center">
       <h1><b>404 - Page Not Found</b></h1>
       <h5 className="underline underline-offset-1 hover:underline-offset-2 hover:text-blue-600">
-        <a href="/">back to home</a>
+        <Link href="/">back to home</Link>
       </h5>
     </div>
   )
